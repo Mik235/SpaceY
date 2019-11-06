558cd17214e100db5117d1c4db51938fcd51b1c0
MIK235 <mikaAust@gmx.de>
2019-11-06 08:03:31 +0100

    ah YES

--- contact.php
+++ contact.php
@@ -1,15 +1,27 @@
+<<?php
+$empfaenger = 'sebastian.brochaus@edu.bib.de';
+$betreff = 'Newsletter Bestätigung';
+$nachricht = 'Hallo, Danke für die Regestierung des Newsletters. ';
+$header = 'From: noreply@SpaceY.space' . "\r\n" .
+'Reply-To: noreply@SpaceY.space' . "\r\n" .
+  'X-Mailer: PHP/' . phpversion();
+
+mail($empfaenger, $betreff, $nachricht, $header);
+
+ ?>
+
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
   <head>
-    <title>Vacation - Free Bootstrap 4 Template by Colorlib</title>
+    <title>Space Y Kontakt</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
 
     <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
     <link rel="stylesheet" href="css/animate.css">
-    
+
     <link rel="stylesheet" href="css/owl.carousel.min.css">
     <link rel="stylesheet" href="css/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/magnific-popup.css">
@@ -21,7 +33,7 @@
     <link rel="stylesheet" href="css/bootstrap-datepicker.css">
     <link rel="stylesheet" href="css/jquery.timepicker.css">
 
-    
+
     <link rel="stylesheet" href="css/flaticon.css">
     <link rel="stylesheet" href="css/icomoon.css">
     <link rel="stylesheet" href="css/style.css">
@@ -48,7 +60,7 @@
 	    </div>
 	  </nav>
     <!-- END nav -->
-    
+
     <section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
@@ -103,7 +115,7 @@
         </div>
       </div>
     </section>
-			
+
 		<section class="ftco-section contact-section">
       <div class="container">
         <div class="row block-9">
@@ -125,7 +137,7 @@
                 <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
               </div>
             </form>
-          
+
           </div>
 
           <div class="col-md-6 d-flex">
@@ -198,8 +210,8 @@
         </div>
       </div>
     </footer>
-    
-  
+
+
 
   <!-- loader -->
   <div id="ftco-loader" class="show fullscreen"><svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/></svg></div>
@@ -221,6 +233,6 @@
   <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false"></script>
   <script src="js/google-map.js"></script>
   <script src="js/main.js"></script>
-    
+
   </body>
 </html>
