3d050d9fc98f931c37e81825f0bb0146a0130452
MIK235 <mikaAust@gmx.de>
2020-01-17 10:01:45 +0100

    Nessus Bild

--- index.php
+++ index.php
@@ -276,7 +276,7 @@
         	</div>
         	<div class="col-md-3 ftco-animate">
         		<div class="project-destination">
-        			<a href="#" class="img" style="background-image: url(images/place-3.jpg);">
+        			<a href="#" class="img" style="background-image: url(images/Nessus.png);">
         				<div class="text">
         					<h3>Nessus Watchers Grave</h3>
         					<span>5 Touren</span>
